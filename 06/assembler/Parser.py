1f6b9eee58e9e798d58ea6d26e0d12fd043a3d91
JennaLH <jenna.howe@live.co.uk>
2015-03-10 13:19:55 -0400

    Add fn to separate out parts of C-instrs

--- 06/assembler/Parser.py
+++ 06/assembler/Parser.py
@@ -29,9 +29,10 @@ self.comp = command[4:9]
 self.dest = command[10:12]
 self.jump = command[13:15]
 """
+import pdb
 
 class Parser:
-    """A class to handle parsing each line of an assembly program"""
+    """A class to parse an assembly program"""
     def __init__(self, assembly_file):
         """Open filestream for assembly file and prepare to parse it."""
         with open(assembly_file, 'r') as raw_code:
@@ -52,6 +53,54 @@ class Parser:
                     code.append(line)
 
             self.code = code
+
+    def set_current_command(self, command_index):
+        """Set which command is being analysed currently."""
+        self.current_command = self.code[command_index]
+
+    def command_type(self):
+        """Return the type of the current command."""
+        if self.current_command[0] == '@':
+            command_type = 'A_COMMAND'
+        elif self.current_command[0] == '(':
+            command_type = 'L_COMMAND'
+        else:
+            command_type = 'C_COMMAND'
+            
+        return command_type
+
+    def c_instr_parts(self):
+        """Return the specified part of self.current_command."""
+        split_equals = self.current_command.split('=')
+        split_semicolon = self.current_command.split(';')
+
+        if len(split_semicolon) == 2:
+            dest = 'null'
+            comp = split_semicolon[0]
+            jump = split_semicolon[1]
+
+            return (dest, comp, jump)
+
+        if len(split_equals) == 2:
+            dest = split_equals[0]
+            comp = split_equals[1]
+            jump = 'null'
+
+            return (dest, comp, jump) 
+        else:
+            print("Oh dear")
+
+
+    def symbol(self):
+        """Return the symbol for an A- or L-instruction."""
+
+
+
+
+
+
+
+
 
 
 
